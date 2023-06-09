4c578631fe3aedd2cf0ffc19e5b85b904d7f3429
momenur <momenurislam6@gmail.com>
2023-06-10 01:25:44 +0600

    login page design

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,19 @@
+import { Link } from 'react-router-dom';
 import './Login.css'
 const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value
+        console.log(email, password);
+    }
     return (
         <div className='loginBG'>
             <div className="min-h-screen bg-black hero bg-opacity-70">
                 <div className="flex-col hero-content ">
                 <h1 className="mb-6 text-5xl font-bold text-neutral-content">Login Now !</h1>
-                    <form className="flex-shrink-0 w-full max-w-sm shadow-2xl card bg-base-100">
+                    <form onSubmit={handleLogin} className="flex-shrink-0 w-full max-w-sm shadow-2xl card bg-base-100">
                         <div className="card-body">
                             <div className="form-control">
                                 <label className="label">
@@ -24,11 +32,12 @@ const Login = () => {
                             </div>
                             <div className="mt-6 form-control">
                                 <input className="btn btn-primary" type="submit" value="Login" />
+                                <p className='mt-4'><small>All Ready Have an Account</small> <Link to='signUp'><span className='font-semibold underline text-sky-600'>Sign Up</span></Link></p>
                             </div>
                         </div>
                     </form>
-                    <button className='btn btn-primary'> Sign In With Google</button>
-                            
+                    <button className='w-full -mt-3 btn btn-primary'> Sign In With Google</button>
+                           
                 </div>
             </div>
         </div>
